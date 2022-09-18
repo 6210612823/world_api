b96b688b6031990601ca2f4f9d428885eeab6f01
superlative <6210612849@student.tu.ac.th>
2022-09-18 13:53:07 +0700

    fixed utf-8

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ import csv
 # See https://fastapi.tiangolo.com/tutorial/first-steps/
 from fastapi import FastAPI
 
-<<<<<<< HEAD
-# get country data from csv file  
+# get country data from csv file
 filename = "world_table_country.csv"
-with open(filename, "r",encoding="UTF-8") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    headers = next(csv_reader)
-    data_country = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
-=======
-# get city data from csv file
-filename = "world_table_city.csv"
 with open(filename, "r", encoding="utf-8") as csv_file:
     csv_reader = csv.reader(csv_file)
     headers = next(csv_reader)
     data_country = [{k: v for (k, v) in zip(headers, row)}
                     for row in csv_reader]
->>>>>>> 18e7736 (Add endpoint /world/city/{name})
+
+# get city data from csv file
+filename = "world_table_city.csv"
+with open(filename, "r", encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    headers = next(csv_reader)
+    data_city = [{k: v for (k, v) in zip(headers, row)} for row in csv_reader]
 
 # create an instance of class FastAPI named "app"
 app = FastAPI()
@@ -31,7 +29,6 @@ app = FastAPI()
 async def read_root() -> dict:
     return {"message": "Hello there"}
 
-<<<<<<< HEAD
 
 # define function that handles "GET" request with endpoint "/world"
 # list all countries
@@ -39,15 +36,12 @@ async def read_root() -> dict:
 async def read_countries() -> dict:
     return {"result": data_country}
 
-=======
+
 # define function that handles "GET" request with endpoint "/world/city/{name}"
 # "/world/city/{name}" is a "path paramter" endpoint
-
-
 @app.get("/world/city/{name}")
 async def read_city(name: str) -> dict:
     for row in data_city:
         if row["Name"].lower() == name.lower():
             return {"result": row}
     return {"result": {}}
->>>>>>> 18e7736 (Add endpoint /world/city/{name})
